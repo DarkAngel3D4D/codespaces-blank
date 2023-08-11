8d5bc97edfc45060cfa3bf0fa56ef1613cc3b73e
DarkAngel3D4D <103005247+DarkAngel3D4D@users.noreply.github.com>
2023-08-11 01:12:10 +0100

    Update random sentences generator.py

--- random sentences generator.py
+++ random sentences generator.py
@@ -1,4 +1,6 @@
 import random
 
 verbs = ["be","have","do","say","go","get","make","know"]
-print(random.choice(verbs))
+names = ["Dave", "Kelly", "Toto", "Scar", "Mufasa", "Lilo", "Oscar", "Dodo"]
+activities = ["ski", "bake", "code", "gaming", "baskeball"]
+print(random.choice(names), random.choice(verbs), random.choice(activities))
